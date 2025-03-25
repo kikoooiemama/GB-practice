02ec2a022e63b248bc61e13508cafad5755b7d74
pakhomov <pakhomov@anispro.ru>
2025-03-25 14:35:58 +0300

    commit part 7

--- Others/Python algorithms/07/common/task_5.py
+++ Others/Python algorithms/07/common/task_5.py
@@ -1,1 +1,49 @@
 # Created by Nikolay Pakhomov 27.02.2025
+# Быстрая сортировка (Хоара)
+# 1. Выбираем опорный элемент (pivot)
+# 2. Сравниваем элементы массива с опорным и переставляем их так, чтобы разбить массив на три непрерывных отрезка:
+# "меньшие опорного", "равные" и "большие".
+# 3. Для отрезков меньше и больше рекурсивно выполнить сортировку.
+
+
+import random
+
+size = 10
+array = [i for i in range(size)]
+random.shuffle(array)
+print(array)
+
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot = random.choice(array)
+    s_ar = []
+    m_ar = []
+    l_ar = []
+    for item in array:
+        if item < pivot:
+            s_ar.append(item)
+        elif item > pivot:
+            l_ar.append(item)
+        elif item == pivot:
+            m_ar.append(item)
+        else:
+            raise Exception("Случилось непредвиденное")
+
+    return quick_sort(s_ar) + m_ar + quick_sort(l_ar)
+
+
+def quick_sort_short(array):
+    if len(array) < 1:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quick_sort_short(less) + [pivot] + quick_sort_short(greater)
+
+
+# print(quick_sort_short(array))
+print(quick_sort(array))
--- Others/Python algorithms/07/common/task_6.py
+++ Others/Python algorithms/07/common/task_6.py
@@ -1,1 +1,37 @@
 # Created by Nikolay Pakhomov 27.02.2025
+# Разворот массива.
+# Сортировка Python - используется Timsort (2002 год).
+# Использует вставку + Слияние. Сложность O(n log n)
+# Потребляет память O(n)
+
+import random
+
+size = 10
+array = [i for i in range(size)]
+random.shuffle(array)
+print(array)
+
+
+def revers(array):
+    for i in range(len(array) // 2):
+        array[i], array[len(array) - i - 1] = array[len(array) - i - 1], array[i]
+
+
+revers(array)
+print(array)
+
+array.reverse()
+print(array)
+
+array.sort()
+print(array)
+
+array.sort(reverse=True)
+print(array)
+
+print('*' * 50)
+t = tuple(random.randint(0, 100) for _ in range(size))
+print(t)
+
+t = tuple(sorted(t, revers=True))
+print(t)
